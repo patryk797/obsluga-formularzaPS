c8fd6a20e68acbb83f9456b3a0eee6e413bb6415
patryk797 <patrykskoczynski453@gmail.com>
2022-01-14 10:37:19 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log('WARSZTAT - Obsługa formularza')
+
+const submitForm = (event) => {
+    event.preventDefault();
+
+    let firstName = document.querySelector('[name="fname"]');
+    let lastName = document.querySelector('[name="lname"]');
+
+    console.log(`Imie: ${firstName.value}, Nazwisko: ${lastName.value}`);
+}
+
+
+let form = document.getElementById('form');
+
+form.addEventListener('submit', submitForm);
+
+console.log(form)
